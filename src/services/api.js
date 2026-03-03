3d36aec7d33f31c474d8ab882e9d0741d65fdd1e
Poojan Patel <poojan.patel@hitechdigital.com>
2026-03-03 09:13:42 +0530

    vercel config

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://shopelite-backend-8skx.onrender.com/api/v1'
+const BASE_URL = 'https://shopelite-backend-8skx.onrender.com/api/v1'
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
